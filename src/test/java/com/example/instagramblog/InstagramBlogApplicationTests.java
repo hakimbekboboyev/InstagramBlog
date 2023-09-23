d34eea589c09dc2c296ce08ed2523f77a2a70ac0
codemylove <hakimbekboboyev@gmail.com>
2023-09-23 22:11:14 +0500

    Register jwt success commit
    Signed-off-by: codemylove <hakimbekboboyev@gmail.com>

---
+++ src/test/java/com/example/instagramblog/InstagramBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instagramblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
